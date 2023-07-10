3312d6db9383613322ea3f8febf0681f8269ce46
raulenzo <raru69@hotmail.com>
2023-07-10 22:32:18 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Modal from 'react-modal';
 
 function App() {
+
   const [characters, setCharacters] = useState([]);
   const [selectedSpecies, setSelectedSpecies] = useState("");
   const [selectedType, setSelectedType] = useState("");
@@ -10,7 +11,7 @@ function App() {
   const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=200")
       .then(response => response.json())
       .then(data => {
         const promises = data.results.map(result =>
@@ -24,9 +25,11 @@ function App() {
   }, []);
 
   const handleSpeciesChange = (event) => {
-    setSelectedSpecies(event.target.value);
+    setSelectedSpecies(event.target.value.toLowerCase());
   };
+  
 
+  
   const handleTypeChange = (event) => {
     setSelectedType(event.target.value);
   };
@@ -44,7 +47,7 @@ function App() {
   };
 
   const filteredCharacters = characters.filter(character => {
-    if (selectedSpecies && !character.name.includes(selectedSpecies)) {
+    if (selectedSpecies && !character.name.startsWith(selectedSpecies)) {
       return false;
     }
     if (selectedType && !character.types.some(type => type.type.name === selectedType)) {
@@ -63,7 +66,14 @@ function App() {
         <label htmlFor="species">Search by Name:</label>
         <input type="text" id="species" value={selectedSpecies} onChange={handleSpeciesChange} />
         <label htmlFor="type">Filter by Type:</label>
-        <input type="text" id="type" value={selectedType} onChange={handleTypeChange} />
+        <select id="type" value={selectedType} onChange={handleTypeChange}>
+          <option value="">All</option>
+          {characters.map(character => (
+            character.types.map(type => (
+              <option key={type.type.name} value={type.type.name}>{type.type.name}</option>
+            ))
+          ))}
+        </select>
         <label htmlFor="number">Filter by Number:</label>
         <input type="text" id="number" value={selectedNumber} onChange={handleNumberChange} />
       </div>
@@ -71,15 +81,16 @@ function App() {
         {filteredCharacters.map(character => (
           <div key={character.name} className="card" onClick={() => handleCardClick(character)}>
             <h2>{character.name}</h2>
+            <img src={character.sprites.front_default} alt={character.name} />
             <p>Species: {character.species.name}</p>
             <p>Type: {character.types.map(type => type.type.name).join(", ")}</p>
-            <img src={character.sprites.front_default} alt={character.name} />
+            
             <p>Number: {character.id}</p>
             <p>Weight: {character.weight}</p>
             <p>Habilidades: {character.abilities.map(ability => ability.ability.name).join(", ")}</p>
             <p>Estadísticas: {character.stats.map(stat => `${stat.stat.name}: ${stat.base_stat}`).join(", ")}</p>
-            <p>Estadísticas  combate: Ataque: {character.attack}, Defensa: {character.defense}, Velocidad: {character.speed}</p>
-            </div>
+            <p>Estadísticas combate: Ataque: {character.attack}, Defensa: {character.defense}, Velocidad: {character.speed}</p>
+          </div>
         ))}
       </div>
       <Modal
@@ -95,6 +106,7 @@ function App() {
              <img src={selectedCharacter.sprites.front_default} alt={selectedCharacter.name} />
              <p>Type: {selectedCharacter.types.map(type => type.type.name).join(", ")}</p>
              <p>Species: {selectedCharacter.species.name}</p>
+            
              <button className="close-button" onClick={handleCloseModal}>
                Close
              </button>
@@ -106,4 +118,3 @@ function App() {
 }
 
 export default App;
-
